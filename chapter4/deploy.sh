42c7db151ca0bffd5b51dfd2b18a52c462cab8ca
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2020-08-05 18:55:55 +0200

    Chapter 4 ready

--- chapter4/deploy.sh
+++ chapter4/deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 RANDOM=$$
 BUCKET_NAME=bucketname-$RANDOM
+STAGE_NAME=dev
+AWS_REGION=$(aws configure list | grep region | awk '{print $2}')
 aws s3 mb s3://$BUCKET_NAME
 echo $BUCKET_NAME > bucket-name.txt
 aws s3 cp openapi.yaml s3://$BUCKET_NAME/openapi.yaml
 sam deploy --s3-bucket $BUCKET_NAME --stack-name chapter4-stack --capabilities CAPABILITY_IAM --parameter-overrides BucketName=$BUCKET_NAME
 aws cloudformation wait stack-create-complete --stack-name chapter4-stack
 API_ID=$(aws apigateway get-rest-apis --query "items[?name=='send-money-api'].id" --output text)
-aws apigateway create-deployment --rest-api-id $API_ID --stage-name dev
-#DEPLOYMENT_ID=$(aws apigateway get-stage --rest-api-id $API_ID --stage-name dev --query "deploymentId" --output text)
-#aws apigateway get-deployment --rest-api-id $API_ID --deployment-id $DEPLOYMENT_ID
+aws apigateway create-deployment --rest-api-id $API_ID --stage-name $STAGE_NAME >/dev/null 2>&1
+echo "Your API with ID $API_ID is deployed and ready to be tested at https://$API_ID.execute-api.$AWS_REGION.amazonaws.com/$STAGE_NAME"
+
