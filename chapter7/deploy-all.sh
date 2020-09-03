1d0fc9d07ca24a8251976c33d298b4ed4c73b5b5
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2020-09-03 12:31:36 +0200

    Have amended deploy-all.sh

--- chapter7/deploy-all.sh
+++ chapter7/deploy-all.sh
@@ -9,7 +9,7 @@ aws s3 cp openapi.yaml s3://$BUCKET_NAME/openapi.yaml
 sam deploy --template-file ../chapter6/template.yaml --s3-bucket $BUCKET_NAME --stack-name chapter6-stack --capabilities CAPABILITY_IAM --parameter-overrides BucketName=$BUCKET_NAME
 API_ID=$(aws apigateway get-rest-apis --query "items[?name=='send-money-api'].id" --output text)
 aws apigateway create-deployment --rest-api-id $API_ID --stage-name $STAGE_NAME >/dev/null 2>&1
-echo "api-url=https://$API_ID.execute-api.$AWS_REGION.amazonaws.com/$STAGE_NAME" > ../chapter5/src/main/resources/aws-lambda.properties
+echo "api-url=https://$API_ID.execute-api.$AWS_REGION.amazonaws.com/$STAGE_NAME/orders" > ../chapter5/src/main/resources/aws-lambda.properties
 sam deploy --template-file ../chapter5/template.yaml --s3-bucket $BUCKET_NAME --stack-name chapter5-stack --capabilities CAPABILITY_IAM --parameter-overrides BucketName=$AWS_REGION-$STAGE_NAME-$BUCKET_NAME
 aws cloudformation wait stack-create-complete --stack-name chapter5-stack
 sam deploy --s3-bucket $BUCKET_NAME --stack-name chapter7-stack --capabilities CAPABILITY_IAM
