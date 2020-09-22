7d84d5c7fc5934f0d0ff56590c2b6d692a719905
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2020-09-22 16:28:40 +0200

    Metrics ready

--- chapter10/src/main/java/fr/simplex_software/aws/lambda/quarkus/CustomerResource.java
+++ chapter10/src/main/java/fr/simplex_software/aws/lambda/quarkus/CustomerResource.java
@@ -1,6 +1,9 @@
 package fr.simplex_software.aws.lambda.quarkus;
 
 import lombok.extern.slf4j.*;
+import org.eclipse.microprofile.metrics.*;
+import org.eclipse.microprofile.metrics.annotation.*;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
 
 import javax.annotation.security.*;
 import javax.enterprise.context.*;
@@ -12,14 +15,14 @@ import java.util.*;
 @Path("/customers")
 @RequestScoped
 @Produces(MediaType.APPLICATION_JSON)
-@Slf4j
 public class CustomerResource
 {
   @Inject
   private CustomerService customerService;
 
   @GET
-  @PermitAll
+  @Counted(name = "customersCount", description = "How many times this method is called.")
+  @Timed(name = "customersTimer", description = "How long it takes to execute the method.", unit = MetricUnits.MILLISECONDS)
   public List<Customer> getCustomers()
   {
     return customerService.getCustomers();
@@ -27,7 +30,8 @@ public class CustomerResource
 
   @Path("/{id}")
   @GET
-  @RolesAllowed({ "User", "Admin" })
+  @Counted(name = "customerByIdCount", description = "How many times this method is called.")
+  @Timed(name = "customerByIdTimer", description = "How long it takes to execute the method.", unit = MetricUnits.MILLISECONDS)
   public Response getCustomerById(@PathParam("id") int id)
   {
     return Response.ok(customerService.getCustomerById(id)).build();
@@ -35,7 +39,8 @@ public class CustomerResource
 
   @Path("/customer/{name}")
   @GET
-  @RolesAllowed({ "Admin" })
+  @Counted(name = "customersByLastNameCount", description = "How many times this method is called.")
+  @Timed(name = "customersByLastNameTimer", description = "How long it takes to execute the method.", unit = MetricUnits.MILLISECONDS)
   public Response getCustomersByLastName(@PathParam("name") String lastName)
   {
     return Response.ok(customerService.getCustomersByLastName(lastName)).build();
--- chapter10/src/test/java/fr/simplex_software/aws/lambda/quarkus/TestCustomerResource.java
+++ chapter10/src/test/java/fr/simplex_software/aws/lambda/quarkus/TestCustomerResource.java
@@ -1,12 +1,10 @@
 package fr.simplex_software.aws.lambda.quarkus;
 
 import io.quarkus.test.junit.*;
-import io.restassured.response.*;
-import io.smallrye.jwt.build.*;
+import io.restassured.http.*;
 import org.junit.jupiter.api.*;
 
 import javax.inject.*;
-import java.util.*;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -16,27 +14,38 @@ public class TestCustomerResource
 {
   @Inject
   CustomerResource customerResource;
-  //private String token = Jwt.issuer("CN=nduminil, O=SimplexSoftware, C=FR").upn("nduminil").groups(new HashSet<>(Arrays.asList("User", "Admin"))).sign();
 
   @Test
   public void testGetCustomerById()
   {
-    //given().header("Authorization", "Bearer " + token).when().
-      get("/customers/1").then().statusCode(200).body("firstName", equalTo("Robert"));
+    get("/customers/1").then().statusCode(200).body("firstName", equalTo("Robert"));
+    assertMetricValue("fr.simplex_software.aws.lambda.quarkus.CustomerResource.customerByIdCount", 1);
+    get("/customers/1").then().statusCode(200).body("firstName", equalTo("Robert"));
+    assertMetricValue("fr.simplex_software.aws.lambda.quarkus.CustomerResource.customerByIdCount", 2);
   }
 
   @Test
   public void testGetCustomersByLastName()
   {
-    //given().header("Authorization", "Bearer " + token).when().
-      get("/customers/customer/Smith").then().statusCode(200).body("size()", is(1))
+    get("/customers/customer/Smith").then().statusCode(200).body("size()", is(1))
       .extract().response().body().jsonPath().getList(".", Customer.class).get(0).getFirstName().equals("Robert");
+    assertMetricValue("fr.simplex_software.aws.lambda.quarkus.CustomerResource.customersByLastNameCount", 1);
+    get("/customers/customer/Smith").then().statusCode(200).body("size()", is(1))
+      .extract().response().body().jsonPath().getList(".", Customer.class).get(0).getFirstName().equals("Robert");
+    assertMetricValue("fr.simplex_software.aws.lambda.quarkus.CustomerResource.customersByLastNameCount", 2);
   }
 
   @Test
   public void testGetCustomers()
   {
-    Response response = given().when().get("/customers").andReturn();
-    response.then().statusCode(200).body("id", hasItems(1, 2, 3, 4)).body("firstName", hasItems("Robert", "Jane", "Fabienne", "Brigitte"));
+    get("/customers").andReturn().then().statusCode(200).body("id", hasItems(1, 2, 3, 4)).body("firstName", hasItems("Robert", "Jane", "Fabienne", "Brigitte"));
+    assertMetricValue("fr.simplex_software.aws.lambda.quarkus.CustomerResource.customersCount", 1);
+    get("/customers").andReturn().then().statusCode(200).body("id", hasItems(1, 2, 3, 4)).body("firstName", hasItems("Robert", "Jane", "Fabienne", "Brigitte"));
+    assertMetricValue("fr.simplex_software.aws.lambda.quarkus.CustomerResource.customersCount", 2);
+  }
+
+  private void assertMetricValue(String metric, Object value)
+  {
+    given().header(new Header("Accept", "application/json")).get("/metrics/application").then().statusCode(200).body("'" + metric + "'", is(value));
   }
 }
